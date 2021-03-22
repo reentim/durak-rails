e35bc02d282ed553f3cc995df77d73e8a12cf0e6
Timothy Reen <reentim@gmail.com>
2021-03-22 12:35:38 +1100

    When conceding, pick up defence cards as well as attack

--- app/controllers/concessions_controller.rb
+++ app/controllers/concessions_controller.rb
@@ -4,7 +4,7 @@ class ConcessionsController < ApplicationController
     player = Player.find_by(secret: cookies[:_durak_player_secret])
 
     if game.state['defender'] == player.id
-      game.state['hands'][player.id].concat(game.state['attacks'].compact)
+      game.state['hands'][player.id].concat(game.state['attacks'], game.state['defences'])
       game.conceede_round
       game.save!
 
