bf246edd57d6893d439bb2c3a8aa6b15fc7bbf2f
Timothy Reen <reentim@gmail.com>
2021-03-24 16:26:30 +1100

    Update ActionCable config for production

--- app/javascript/src/cable.js
+++ app/javascript/src/cable.js
@@ -1,6 +1,6 @@
 import { createConsumer } from '@rails/actioncable'
 
-const URL = 'ws://localhost:3000/cable'
+const URL = 'wss://durak-rails.herokuapp.com/cable'
 const consumer = createConsumer(URL)
 
 export default consumer
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,8 +40,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.url = 'wss://durak-rails.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [ 'https://durak-rails.herokuapp.com', /http:\/\/durak-rails.herokuapp.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
